506ea8e6322b012fe9a55b79084eb6d9a2425db0
byoms23 <byoms23@gmail.com>
2010-08-30 22:23:27 -0600

    Add suport for the graphical interface of the gates.

--- src/main/scala/Componentes.scala
+++ src/main/scala/Componentes.scala
@@ -4,18 +4,28 @@ import main._;
 import scala.swing._
 import scala.swing.event._
 
-trait Componente
+trait Componente {
+	var posX : Int
+	var posY : Int
+	var name : String
+}
 
 class GraphicalCircuit(var inputNames:List[GraphicalInput],var gates:List[Gate], var outputName:String)
 
-class GraphicalBinaryGate(var gName: String, var gInput1Name:String, val gInput2Name: String, val gDelaySpec:BinaryDelaySpec, val gOperationName:String, var posX : Int, var posY : Int) extends BinaryGate (gName, gInput1Name, gInput2Name, gDelaySpec, gOperationName) with Componente {
+class GraphicalBinaryGate(override var name: String, var gInput1Name:String, val gInput2Name: String, val gDelaySpec:BinaryDelaySpec, val gOperationName:String, var posX : Int, var posY : Int) extends BinaryGate (name, gInput1Name, gInput2Name, gDelaySpec, gOperationName) with Componente {
 	
 }
 
-class GraphicalUnaryGate(val gName: String, val gInputName:String, val gDelaySpec:UnaryDelaySpec, val gOperationName:String, var posX : Int, var posY : Int) extends UnaryGate (gName, gInputName, gDelaySpec, gOperationName) with Componente {
+class GraphicalUnaryGate(override var name: String, val gInputName:String, val gDelaySpec:UnaryDelaySpec, val gOperationName:String, var posX : Int, var posY : Int) extends UnaryGate (name, gInputName, gDelaySpec, gOperationName) with Componente {
 	
 }
 
 class GraphicalInput(var name: String, var posX : Int, var posY : Int) extends Componente {
 	
+}
+
+class ComponenteGrafico(val c : Componente) extends Component {
+	var posX : Int = c.posX
+	var posY : Int = c.posY
+	var name : String = c.name
 }
